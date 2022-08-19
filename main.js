d15d662eb9c80435af5ed640190a74378310522e
bzceval <busraaceval@gmail.com>
2022-08-19 04:00:45 +0300

    remove am-pm

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 const hourEl = document.getElementById("hour");
 const minuteEl = document.getElementById("minutes");
 const secondEl = document.getElementById("seconds");
-const ampmEl = document.getElementById("ampm");
+// const ampmEl = document.getElementById("ampm");
 
 function updateClock() {
   //ilk önce PC saati alıyor. Aldığı saati bi değişkene atıyor
   let h = new Date().getHours();
   let m = new Date().getMinutes();
   let s = new Date().getSeconds();
-  let ampm = "AM";
+  // let ampm = "AM";
 
-  if (h > 12) {
-    h = h - 12;
-    ampm = "PM";
-  }
+  // if (h > 12) {
+  //   h = h - 12;
+  //   ampm = "PM";
+  // }
 
   //digital saat yaptığımzıdan 0 ekler
   h = h < 10 ? "0" + h : h;
@@ -25,7 +25,7 @@ function updateClock() {
   hourEl.innerText = h;
   minuteEl.innerText = m;
   secondEl.innerText = s;
-  ampmEl, (innerText = ampm);
+  // ampmEl, (innerText = ampm);
 
   //counter dönüyor
   setTimeout(() => {
